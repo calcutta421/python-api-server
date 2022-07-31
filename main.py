ed58314c1978d24bef19b8a842fd7ec465a90ffa
Zirou Kamikaze <kamikaze.zirou@gmail.com>
2022-08-01 01:26:13 +0900

    tutorial4: Basic認証

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
-import string
-from fastapi import FastAPI
+import os
+import secrets
+
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 app = FastAPI()
 
+security = HTTPBasic()
+
+class B(Exception):
+    pass
+
+USERNAME = os.getenv("USERNAME")
+PASSWORD = os.getenv("PASSWORD")
+if not USERNAME or not PASSWORD:
+    raise Exception('credentials is not set.')
+
+def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, USERNAME)
+    correct_password = secrets.compare_digest(credentials.password, PASSWORD)
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
 
 @app.get("/")
-async def root(q: str):
+async def root(username: str = Depends(get_current_username), q: str = ""):
     return {"message": q}
